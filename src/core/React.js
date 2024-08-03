9a48aeeba81efc0903484e6496912ddb880c5fec
aoshisen <aoshisen.23@gmail.com>
2024-08-03 11:40:10 +0800

    feat: 实现fiber架构

--- src/core/React.js
+++ src/core/React.js
@@ -1,6 +1,7 @@
+let nextFiber = null;
 function createTextNode(text) {
 	return {
-		type: "TEXT_NODE",
+		type: "TEXT_ELEMENT",
 		props: {
 			nodeValue: text,
 			children: []
@@ -22,14 +23,78 @@ function createElement(type, props, ...children) {
 }
 
 function render(el, container) {
-	const { props: { children, nodeValue, ...resetProps }, type } = el;
-	const dom = type === "TEXT_NODE" ? document.createTextNode(nodeValue) : document.createElement(type);
-	Object.keys(resetProps).forEach(key => {
-		dom.setAttribute(key, resetProps[key])
-	})
-	children.map(child => render(child, dom))
-	container.append(dom)
+	//在这里去初始化 我们的任务
+	nextFiber = {
+		dom: container,
+		props: {
+			children: [el]
+		}
+	}
 }
+
+function createDom(type) {
+	return type === "TEXT_ELEMENT" ? document.createTextNode("") : document.createElement(type);
+}
+function updateProps(dom, props) {
+	Object.keys(props).forEach(key => {
+		if (key !== "children") {
+			dom[key] = props[key];
+		}
+	})
+}
+function initChildren(fiber) {
+	// 3. 建立当前的dom 元素的子父级,兄弟节点的关系
+	const children = fiber.props.children;
+	// 记录上一个节点, 方便给上一个节点绑定上sibling 属性
+	let prevChild = null;
+	children.forEach((child, index) => {
+		let newFiber = {
+			type: child.type,
+			props: child.props,
+			sibling: null,
+			child: null,
+			parent: fiber,
+			dom: null
+		}
+		if (index === 0) {
+			fiber.child = newFiber;
+		}
+		else {
+			prevChild.sibling = newFiber;
+		}
+		prevChild = newFiber;
+	});
+}
+
+function performWorkUnit(fiber) {
+	if (!fiber.dom) {
+		const dom = fiber.dom = createDom(fiber.type)
+		console.log(dom)
+		fiber.parent.dom.append(dom)
+		updateProps(dom, fiber.props)
+	}
+	initChildren(fiber)
+	if (fiber.child) {
+		return fiber.child;
+	}
+	if (fiber.sibling) {
+		return fiber.sibling;
+	}
+	return fiber.parent?.sibling;
+}
+function workerLoop(IdleDeadline) {
+	let shouldYield = false;
+	while (!shouldYield && nextFiber) {
+		const remaining = IdleDeadline.timeRemaining()
+		nextFiber = performWorkUnit(nextFiber)
+		if (remaining < 1) {
+			shouldYield = true;
+		}
+	}
+	requestIdleCallback(workerLoop)
+}
+
+requestIdleCallback(workerLoop);
 
 const React = {
 	render, createElement
--- src/core/ReactDOM.js
+++ src/core/ReactDOM.js
@@ -2,6 +2,7 @@ import React from './React.js'
 // createRoot 函数返回一个render 方法,render 方法传入一个element 然后会挂载到对应的节点上
 function createRoot(root) {
 	function render(App) {
+		console.log(App, "<<<<<")
 		React.render(App, root)
 	}
 	return {
